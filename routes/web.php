def532bb992eecb62054f32e5977699cccf1ec8a
umeduck <masa.0116>
2023-06-25 14:16:20 +0900

    [add]40

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\InertiaTestController;
+use App\Http\Controllers\ItemController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('items',ItemController::class)
+->middleware(['auth','verified']);
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
